e2d7f416fa706e937eead149861d297dc3384342
Al-Nafis <abidalnafis@yahoo.com>
2021-08-20 10:56:18 -0500

    used coroutine with retrofit

--- app/src/main/java/com/mnafis/restapitesting/omdb/OmdbManager.kt
+++ app/src/main/java/com/mnafis/restapitesting/omdb/OmdbManager.kt
@@ -1,9 +1,9 @@
 package com.mnafis.restapitesting.omdb
 
-import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,28 +11,33 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class OmdbManager {
 
-    private val service = Retrofit.Builder()
+    private val rxService = Retrofit.Builder()
         .baseUrl("https://omdbapi.com")
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(OmdbService::class.java)
 
-    fun getMovieDetails(name: String): Single<MovieResponse> {
-        return service.getMovieInfo("34737eb2", name, "movie").subscribeOn(Schedulers.io())
+    fun getMovieDetailsRx(name: String): Single<MovieResponse> {
+        return rxService.getMovieInfoRx("34737eb2", name, "movie").subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
 
-    fun getMovieList(searchKey: String): Single<MovieListResponse> {
-        return service.getMovieList("34737eb2", searchKey, "movie").subscribeOn(Schedulers.io())
+    fun getMovieListRx(searchKey: String): Single<MovieListResponse> {
+        return rxService.getMovieListRx("34737eb2", searchKey, "movie").subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
 
-    private fun <T> Single<T>.getLoadedSingle(): Single<T> {
-        return subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-    }
+    private val coroutineService = Retrofit.Builder()
+        .baseUrl("https://omdbapi.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(OmdbService::class.java)
 
-    private fun <T> Observable<T>.getLoadedSingle(): Observable<T> {
-        return subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    suspend fun getMovieDetailsCoroutine(name: String): Response<MovieResponse> =
+            rxService.getMovieInfoCoroutine ("34737eb2", name, "movie")
+
+    suspend fun getMovieListCoroutine(searchKey: String): Response<MovieListResponse> {
+        return rxService.getMovieListCoroutine("34737eb2", searchKey, "movie")
     }
 }
--- app/src/main/java/com/mnafis/restapitesting/omdb/OmdbService.kt
+++ app/src/main/java/com/mnafis/restapitesting/omdb/OmdbService.kt
@@ -1,21 +1,36 @@
 package com.mnafis.restapitesting.omdb
 
 import io.reactivex.Single
+import retrofit2.Response
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface OmdbService {
     @POST("/")
-    fun getMovieInfo(
+    fun getMovieInfoRx(
         @Query("apikey") key: String,
         @Query("t") title: String,
         @Query("type") type: String
     ): Single<MovieResponse>
 
     @POST("/")
-    fun getMovieList(
+    fun getMovieListRx(
         @Query("apikey") key: String,
         @Query("s") searchKey: String,
         @Query("type") type: String
     ): Single<MovieListResponse>
+
+    @POST("/")
+    suspend fun getMovieInfoCoroutine(
+        @Query("apikey") key: String,
+        @Query("t") title: String,
+        @Query("type") type: String
+    ): Response<MovieResponse>
+
+    @POST("/")
+    suspend fun getMovieListCoroutine(
+        @Query("apikey") key: String,
+        @Query("s") searchKey: String,
+        @Query("type") type: String
+    ): Response<MovieListResponse>
 }
